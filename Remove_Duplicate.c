1a6818fdabe60c41b86083640fbc2919992ebcd4
Sanchi2610 <sanchigaikwad2610@gmail.com>
2025-09-23 18:25:08 +0530

    RemoveDuplicate

---
+++ Remove_Duplicate.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char string[20]="Sanchisanchi";
+    for(int i=0;string[i]!='\0';i++)
+    {
+        for(int j=i+1;string[j]!='\0';j++)
+        {
+            if(string[i]==string[j])
+            {
+                for(int k=j;string[k]!='\0';k++)
+                {
+                    string[k]=string[k+1];
+                }
+            }
+        }
+    }
+    printf("%s",string);
+    
+}
