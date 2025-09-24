a94d05d6cd30e5794e9021bfc504bdcbb0048dd2
Sanchi2610 <sanchigaikwad2610@gmail.com>
2025-09-24 19:00:46 +0530

    RemoveVowelUsingFunction

---
+++ String/Remove_Vowel_UsingFunction.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include <string.h>
+char remove_vowel(char[ ]);
+int main()
+{
+    char str[10]="sanchi";
+    remove_vowel(str);
+    printf("%s",str);
+    return 0;
+}
+char remove_vowel(char str[ ])
+{
+    int j=0;
+    for(int i=0;str[i]!='\0';i++)
+    {
+        if(str[i]>='a' && str[i]<='z' || str[i]>='A' && str[i]<='Z')
+        {
+            if(str[i]!='a' && str[i]!='e' &&  str[i]!='i' && str[i]!='o' && str[i]!='u' && str[i]!='A' && str[i]!='E' && str[i]!='I' && str[i]!='O' && str[i]!='U')
+            {
+                str[j]=str[i];
+                j++;
+            }
+        }
+    }
+    str[j]='\0';
+}
+   
