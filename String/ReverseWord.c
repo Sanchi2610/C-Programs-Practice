53bb3bc2132dab5193be49c8d6be653aca42c3bd
Sanchi2610 <sanchigaikwad2610@gmail.com>
2025-10-27 15:35:56 +0530

    Update ReverseWord.c

--- String/ReverseWord.c
+++ String/ReverseWord.c
@@ -1,37 +1,36 @@
-
 #include <stdio.h>
-#include<string.h>
-char reverse(char[]);
-char reverse_string(char[],int,int);
-int main()
+#include <string.h>
+void reverse_string(char str[], int l, int r);
+void reverse_words(char str[]);
+int main(void)
 {
-    char str[19]="sanchi pranav";
-    reverse(str);
-    printf("%s",str);
+    char str[50] = "sanchi gaikwad";
+    reverse_string(str, 0, strlen(str) - 1);
+    reverse_words(str);
+    printf("Reversed word order: %s\n", str);
     return 0;
 }
-char reverse_string(char str[],int l,int r)
+void reverse_string(char str[], int l, int r)
 {
-    while(l<r)
+    while (l < r)
     {
-        char temp=str[l];
-        str[l]=str[r];
-        str[r]=temp;
+        char temp = str[l];
+        str[l] = str[r];
+        str[r] = temp;
         l++;
         r--;
     }
 }
-char reverse(char str[])
+void reverse_words(char str[])
 {
-    int len=strlen(str);
-    reverse_string(str,0,len-1);
-    int start=0;
-    for(int i=0;i<=len;i++)
+    int len = strlen(str);
+    int start = 0;
+    for (int i = 0; i <= len; i++)
     {
-        if(str[i]==' ' || str[i]=='\0')
+        if (str[i] == ' ' || str[i] == '\0')
         {
-            reverse_string(str,start,i-1);
-            start=i+1;
+            reverse_string(str, start, i - 1);
+            start = i + 1;
         }
     }
 }
