1c58488facaa321a729e10b566cea18f3cdb0dde
Sanchi2610 <sanchigaikwad2610@gmail.com>
2025-10-27 14:34:26 +0530

    Update Remove_dup.c

--- Remove_dup.c
+++ Remove_dup.c
@@ -1,25 +1,23 @@
-#include <stdio.h>
-#include <string.h>
-
-int main() {
-    char str[10] = "sancnhi";
-    int len = strlen(str);
-
-    for (int i = 0; i < len; i++)
-     {
-        int dup = 0;
-        for (int j = 0; j < i; j++)
-         {
-            if (str[i] == str[j]) 
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char str[20]="sannchi";
+    int len=strlen(str);
+    for(int i=0;i<=len;i++)
+    {
+        int duplicate=0;
+        for(int j=i+1;j<=len;j++)
+        {
+            if(str[i]==str[j])
             {
-                dup = 1;  
-                break;
+                duplicate=1;
             }
         }
-        if (!dup) 
-        {
-            printf("%c", str[i]);
-        }
+    if(!duplicate)
+      {
+        printf("%c",str[i]);
+      }
     }
     return 0;
 }
