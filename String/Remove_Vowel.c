0bc6eef2111ecfe8b835fd3af8e6754319c4076a
Sanchi2610 <sanchigaikwad2610@gmail.com>
2025-10-30 09:41:17 +0530

    no need of j

--- String/Remove_Vowel.c
+++ String/Remove_Vowel.c
@@ -10,18 +10,17 @@ int main()
 }
 char remove_vowel(char str[ ])
 {
-    int j=0;
+    int index=0;
     for(int i=0;str[i]!='\0';i++)
     {
         if(str[i]>='a' && str[i]<='z' || str[i]>='A' && str[i]<='Z')
         {
             if(str[i]!='a' && str[i]!='e' &&  str[i]!='i' && str[i]!='o' && str[i]!='u' && str[i]!='A' && str[i]!='E' && str[i]!='I' && str[i]!='O' && str[i]!='U')
             {
-                str[j]=str[i];
-                j++;
+                str[index++]=str[i];
             }
         }
     }
-    str[j]='\0';
+    str[index]='\0';
 }
    
