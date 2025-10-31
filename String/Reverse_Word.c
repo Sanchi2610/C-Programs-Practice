fb12c41549220b6c633631b48b21be8daeded652
Sanchi2610 <sanchigaikwad2610@gmail.com>
2025-10-31 12:37:51 +0530

    changes

--- String/Reverse_Word.c
+++ String/Reverse_Word.c
@@ -1,16 +1,17 @@
-
-#include <stdio.h>
+#include<stdio.h>
 #include<string.h>
-char reverse(char[]);
-char reverse_string(char[],int,int);
-int main()
+char string_reverse(char[],int,int);
+char word_reverse(char[]);
+int main() 
 {
-    char str[19]="sanchi pranav";
-    reverse(str);
-    printf("%s",str);
-    return 0;
+ char str[20]="sanchi gaikwad"; 
+ int len=strlen(str);
+string_reverse(str,0,len-1);
+ word_reverse(str);
+ printf("%s",str);
+ return 0;
 }
-char reverse_string(char str[],int l,int r)
+char string_reverse(char str[],int l,int r)
 {
     while(l<r)
     {
@@ -21,16 +22,18 @@ char reverse_string(char str[],int l,int r)
         r--;
     }
 }
-char reverse(char str[])
+char word_reverse(char str[])
 {
-    int len=strlen(str);
-    reverse_string(str,0,len-1);
     int start=0;
-    for(int i=0;i<=len;i++)
+    for(int i=0;;i++)
     {
         if(str[i]==' ' || str[i]=='\0')
         {
-            reverse_string(str,start,i-1);
+            string_reverse(str,start,i-1);
+            if(str[i]=='\0')
+            {
+                break;
+            }
             start=i+1;
         }
     }
