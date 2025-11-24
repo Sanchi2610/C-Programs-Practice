db8c52c84387afcfb8e73dbca76be152253fcf37
Sanchi2610 <sanchigaikwad2610@gmail.com>
2025-11-24 20:26:47 +0530

    Update Count_Vowel_Con_Digit_Space.c

--- String/Count_Vowel_Con_Digit_Space.c
+++ String/Count_Vowel_Con_Digit_Space.c
@@ -1,37 +1,37 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<string.h>
-char string(char[]);
-int main() 
+int string(char[]);
+int main()
 {
-    char str[100]="san ch i12";
+    char str[10]="SAn chi12";
     string(str);
     return 0;
 }
-char string(char str[])
+int string(char str[])
 {
-    int vowel=0,consonants=0,space=0,digit=0;
+    int vowel=0,con=0,space=0,digit=0;
     for(int i=0;str[i]!='\0';i++)
     {
-        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u')
-        
-        {
-            vowel++;
-        }
-        else if(str[i]==' ')
-        {
-            space++;
-        }
-        else if(str[i]>='0' && str[i]<='9')
-        {
-            digit++;
-        }
-        else if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
-        {
-            consonants++;
-        }
+            if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
+            {
+                vowel++;
+            }
+             else if(str[i]>='a' && str[i]<='z' || str[i]>='A'&& str[i]<='Z')
+            {
+                con++;
+            }
+            else if (str[i]==' ')
+            {
+                space++;
+            }
+            else if(str[i]>='0' && str[i]<='9')
+            {
+                digit++;
+            }
     }
-    printf("Vowel:%d\n",vowel);
-    printf("Space:%d\n",space);
-    printf("Digits:%d\n",digit);
-    printf("Consonants:%d\n",consonants);
+     printf("Vowel:%d\n",vowel);
+     printf("Con:%d\n",con);
+     printf("Digit:%d\n",digit);
+     printf("Space:%d",space);
 }
+    
