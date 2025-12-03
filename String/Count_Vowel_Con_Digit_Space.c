328ff441e66f7d73a7b1ebaf89f5ca9b829f0699
Sanchi2610 <sanchigaikwad2610@gmail.com>
2025-12-03 21:59:37 +0530

    Update Count_Vowel_Con_Digit_Space.c

--- String/Count_Vowel_Con_Digit_Space.c
+++ String/Count_Vowel_Con_Digit_Space.c
@@ -1,37 +1,39 @@
-// Online C compiler to run C program online
 #include<stdio.h>
 #include<string.h>
-int string(char[]);
+void count(char[]);
 int main()
 {
     char str[10]="SAn chi12";
-    string(str);
+    count(str);
     return 0;
 }
-int string(char str[])
+void count(char str[])
 {
-    int vowel=0,con=0,space=0,digit=0;
+    int vowel=0,con=0,digit=0,space=0;
     for(int i=0;str[i]!='\0';i++)
     {
-            if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
-            {
-                vowel++;
-            }
-             else if(str[i]>='a' && str[i]<='z' || str[i]>='A'&& str[i]<='Z')
-            {
-                con++;
-            }
-            else if (str[i]==' ')
-            {
-                space++;
-            }
-            else if(str[i]>='0' && str[i]<='9')
-            {
-                digit++;
-            }
-    }
-     printf("Vowel:%d\n",vowel);
-     printf("Con:%d\n",con);
-     printf("Digit:%d\n",digit);
-     printf("Space:%d",space);
+     if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
+     {
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' ||str[i]=='o' ||str[i]=='u' ||str[i]=='A' ||str[i]=='E' ||str[i]=='I' ||str[i]=='O' ||str[i]=='U')
+        {
+            vowel++;
+        }
+        else
+        {
+            con++;
+        }
+     }
+        else if(str[i]>='0' && str[i]<='9')
+        {
+           digit++;
+        }
+        else if(str[i]==' ')
+        {
+           space++;
+        }
+      }
+    printf("Number of digits:%d\n",digit);
+    printf("Number of space:%d\n",space);
+    printf("Number of vowels:%d\n",vowel);
+    printf("Number of con:%d",con);
 }
