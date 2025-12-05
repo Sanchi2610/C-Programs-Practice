afbc288cfea9ef0d76ee121ede8a3ba2ba41681b
Sanchi2610 <sanchigaikwad2610@gmail.com>
2025-12-05 14:09:40 +0530

    String_Copy

---
+++ String/String_Copy_Using_Pointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char str1[20]="sanchi";
+    char str2[20]="gaikwad";
+    char *ptr1=str1;
+    char *ptr2=str2;
+    while(*ptr2!='\0')
+    {
+        *ptr1=*ptr2;
+        ptr1++;
+        ptr2++;
+    }
+    *ptr1='\0';
+    printf("%s",str1);
+    return 0;
+}
