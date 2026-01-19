b7319b886f7088e7ddc7747c8544cb624e05c04b
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-01-19 23:57:14 +0530

    Update Compare.c

--- String/Compare.c
+++ String/Compare.c
@@ -1,26 +1,24 @@
 #include <stdio.h>
-#include <string.h>
+
 int main() 
 {
-    char s1[10]="sanchi";
-    int l1=strlen(s1);
-    char s2[10]="sanchI";
-    int l2=strlen(s2);
-    int eq=1;
-    if(l1!=l2)
+    char s1[10] = "sanchi";
+    char s2[10] = "sanchI";
+    int eq = 1;
+
+    for (int i = 0; s1[i] != '\0' || s2[i] != '\0'; i++) 
     {
-        eq=0;
+        if (s1[i] != s2[i]) 
+        {
+            eq = 0;
+            break;
+        }
     }
-    for(int i=0;s1[i]!='\0';i++)
-    {
-            if(s1[i]!=s2[i])
-            {
-                eq=0;
-            }
-    }
-    if(eq)
-    printf("equal");
+
+    if (eq)
+        printf("Equal");
     else
-    printf("Not equal");
+        printf("Not equal");
+
     return 0;
 }
