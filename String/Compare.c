fe8774f68dae2a20e738dc1c485e1a440b20c8db
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-01-19 23:42:42 +0530

    Update Compare.c

--- String/Compare.c
+++ String/Compare.c
@@ -4,7 +4,7 @@ int main()
 {
     char s1[10]="sanchi";
     int l1=strlen(s1);
-    char s2[10]="sanchi";
+    char s2[10]="sanchI";
     int l2=strlen(s2);
     int eq=1;
     if(l1!=l2)
@@ -13,13 +13,10 @@ int main()
     }
     for(int i=0;s1[i]!='\0';i++)
     {
-        for(int j=0;s2[j]!='\0';j++)
-        {
-            if(s1[i]!=s2[j])
+            if(s1[i]!=s2[i])
             {
                 eq=0;
             }
-        }
     }
     if(eq)
     printf("equal");
