79cf35f9931fc4572ee068c0fa4bcfe3b23c6e25
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-01-27 20:18:29 +0530

    Even_Odd_Prime

---
+++ Arrays/Even_Odd_Prime.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+    int a[5]={2,3,1,4,5};
+    int n=sizeof(a)/sizeof(a[0]);
+    for(int i=0;i<n;i++)
+    {
+        int prime=1;
+        if(a[i]<=1)
+        {
+            prime=0;
+        }
+        else
+        {
+            for(int j=2;j<=a[i]/2;j++)
+            {
+                if(a[i]%j==0)
+                {
+                    prime=0;
+                    break;
+                }
+            }
+        }
+        if(prime)
+        printf("%d:Prime\n",a[i]);
+        else
+        {
+           if(a[i]%2==0)
+           {
+            printf("%d:Even\n",a[i]);
+           }
+             if(a[i]%2!=0)
+              {
+                printf("%d:Odd\n",a[i]);
+              }
+        }      
+    }
+    return 0;
+}
