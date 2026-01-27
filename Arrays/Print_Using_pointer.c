eb7146fcb714998e412e0a5137c1b739b5a881ec
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-01-27 16:07:02 +0530

    Print

---
+++ Arrays/Print_Using_pointer.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+   int a[]={1,2,3,4,5,6,7,8};
+   int n=sizeof(a)/sizeof(a[0]);
+   int *p=&a[0];
+   for(int i=0;i<n;i++)
+   {
+       printf("%d",*(p+i));
+   }
+    return 0;
+}
