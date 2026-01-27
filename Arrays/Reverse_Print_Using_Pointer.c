a21fa8bd8c4cf3d8417f82b9f0a542342c1ae0d4
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-01-27 16:13:02 +0530

    Reverse_Array

---
+++ Arrays/Reverse_Print_Using_Pointer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() 
+{
+    int a[ ]={1,2,3,4,5,6,7,8,9};
+    int n=sizeof(a)/sizeof(a[0]);
+    int *p=&a[0];
+    int *q=&a[n-1];
+    while(p<q)
+    {
+        int temp=*p;
+        *p=*q;
+        *q=temp;
+        p++;
+        q--;
+    }
+    for(int i=0;i<n;i++)
+    {
+     printf(" %d",a[i]);
+    }
+    return 0;
+}
