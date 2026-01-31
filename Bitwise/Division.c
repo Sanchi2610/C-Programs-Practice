79d637f25c3f4ac6fef7aaea0a66928d022cc126
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-01-31 21:23:17 +0530

    Div

---
+++ Bitwise/Division.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() 
+{
+   int a,b,q=0;
+   printf("Enter a:");
+   scanf("%d",&a);
+   printf("Enter b:");
+   scanf("%d",&b);
+   while(a>=b)
+   {
+       int temp=b;
+       int count=1;
+       if((b<<1)<=a)
+       {
+           temp=temp<<1;
+           count=count<<1;
+       }
+       a=a-temp;
+       q=q+count;
+}
+printf("Rem:%d\n",a);
+printf("Quo:%d",q);
+return 0;
+}
