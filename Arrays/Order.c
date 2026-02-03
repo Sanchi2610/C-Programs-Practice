7dc9392e157e597f8d1b04634b4831ae1887e1fe
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-02-03 13:46:23 +0530

    Update Order.c

--- Arrays/Order.c
+++ Arrays/Order.c
@@ -1,28 +1,25 @@
+// Online C compiler to run C program online
 #include <stdio.h>
-void order(int [],int n);
 int main()
 {
-   int a[ ]={11,6,8,78};
-   int n=sizeof(a)/sizeof(a[0]);
-   order(a,n);
-   return 0;
-}
-void order(int a[],int n)
-{
-   for(int i=0;i<n-i;i++)
-   {
-       for(int j=0;j<n-i-1;j++)
-       {
-           if(a[j] > a[j+1] )
-           {
-               int temp=a[j];
-               a[j]=a[j+1];
-               a[j+1]=temp;
-           }
-       }
-   }
-  for(int i=0;i<n;i++)
-  {
-       printf(" %d",a[i]);
-  }
+    int a[5]={3,4,1,2,5};
+    int n=sizeof(a)/sizeof(a[0]);
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=0;j<n-1-i;j++)
+        {
+            if(a[j]<a[j+1])
+            {
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+            
+        }
+    }
+    for(int i=0;i<n;i++)
+    {
+     printf("%d ",a[i]);
+    }
+    return 0;
 }
