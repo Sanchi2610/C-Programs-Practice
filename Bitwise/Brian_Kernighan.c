8d805b24cb4f00f12e0283aab1b7cc8ea99e7978
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-02-05 21:54:34 +0530

    Count

---
+++ Bitwise/Brian_Kernighan.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("Enter a:");
+    scanf("%d",&a);
+    int count=0;
+    while(a!=0)
+    {
+        a=a&(a-1);
+        count++;
+    }
+    printf("Count:%d",count);
+    return 0;
+}
