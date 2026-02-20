68684b07240d663d013f53d5a8841fe17d0c38a4
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-02-20 19:28:52 +0530

    pallindrome

---
+++ Linklist/Pallindrome.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+struct node *reverse(struct node *first)
+{
+    struct node *current=first;
+    struct node *previous=NULL;
+    struct node *next=NULL;
+    while(current!=NULL)
+     {
+        next=current->next;
+        current->next=previous;
+        previous=current;
+        current=next;
+     }
+     return previous;
+}
+
+struct node *middle(struct node *first)
+{
+    struct node *slow=first;
+    struct node *fast=first;
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow;
+}
+
+void travel(struct node* ptr)
+{
+    printf("Single Link List:");
+    while(ptr!=NULL)
+    {
+        printf("%d->",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("NULL\n");
+}
+
+int pallindrome(struct node *first)
+{
+    struct node *mid=middle(first);
+    struct node *reversemid=reverse(mid);
+    struct node *p1=first;
+    struct node *p2=reversemid;
+    {
+        if(p1->data!=p2->data)
+        return 0;
+        p1=p1->next;
+        p2=p2->next;
+    }
+    return 1;
+}
+
+int main()
+{
+    struct node *first;
+    struct node *second;
+    struct node *third;
+    
+    first=(struct node*)malloc(sizeof(struct node));
+    second=(struct node*)malloc(sizeof(struct node));
+    third=(struct node*)malloc(sizeof(struct node));
+    
+    first->data=1;
+    first->next=second;
+    
+    second->data=2;
+    second->next=third;
+    
+    third->data=3;
+    third->next=NULL;
+    
+    travel(first);
+    
+    if(pallindrome(first))
+    printf("Pallindrome");
+    else
+    printf("Non-Pallindrome");
+    return 0;
+}
