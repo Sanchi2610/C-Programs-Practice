1cb61415d9803292828886d47bcfa76f81333b20
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-02-27 14:35:41 +0530

    Update Order.c

--- Arrays/Order.c
+++ Arrays/Order.c
@@ -4,9 +4,9 @@ int main()
 {
     int a[5]={3,4,1,2,5};
     int n=sizeof(a)/sizeof(a[0]);
-    for(int i=0;i<n-1;i++)
+    for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n-1-i;j++)
+        for(int j=0;j<n-i;j++)
         {
             if(a[j]<a[j+1])
             {
