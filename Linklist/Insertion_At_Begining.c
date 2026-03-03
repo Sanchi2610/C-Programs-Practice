6f61a423b18bd3c68ceefca334fc18794cdf7731
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-03-03 13:21:46 +0530

    Create Insertion_At_Begining.c

---
+++ Linklist/Insertion_At_Begining.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node 
+{
+    int data;
+    struct node *next;
+};
+
+void travel(struct node *ptr)
+{
+    printf("Single Link List:");
+    while(ptr!=NULL)
+    {
+        printf("%d->",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("NULL\n");
+}
+
+struct node *insertion(struct node *first,int data)
+{
+   struct node *ptr=(struct node*)malloc(sizeof(struct node));
+   ptr->data=data;
+   ptr->next=first;
+   return ptr;
+}
+
+int main() 
+{
+    struct node *first;
+    struct node *second;
+    struct node *third;
+    
+    first=(struct node*)malloc(sizeof(struct node));
+    second=(struct node*)malloc(sizeof(struct node));
+    third=(struct node*)malloc(sizeof(struct node));
+    
+    first->data=1;
+    first->next=second;
+    
+    second->data=2;
+    second->next=third;
+    
+    third->data=3;
+    third->next=NULL;
+    
+    travel(first);
+    first=insertion(first,4);
+    travel(first);
+    
+    return 0;
+}
