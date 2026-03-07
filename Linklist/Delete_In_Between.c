85ab530c77c6f23ed362298725a09d39d7a93f1d
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-03-07 13:56:22 +0530

    Create Delete_In_Between.c

---
+++ Linklist/Delete_In_Between.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;//struct node 
+};
+
+void travel(struct node *ptr)
+{
+    printf("Single Link List:");
+    while(ptr!=NULL)
+    {
+        printf("%d->",ptr->data);
+        ptr=ptr->next;
+    }
+    printf("NULL\n");//travel pointer initalization
+}
+
+struct node *delete(struct node *first,int index)
+{
+    struct node *p=first;
+    int i=0;
+    struct node *q=first->next;
+    while(i!=index-1)
+    {
+        p=p->next;
+        q=q->next;
+        i++;
+    }
+    p->next=q->next;
+    free(q);
+    return first;
+}
+
+int main() 
+{
+    struct node *first;//node declaration
+    struct node *second;
+    struct node *third;
+    
+    first=(struct node*)malloc(sizeof(struct node));//memory allocation from heap
+    second=(struct node*)malloc(sizeof(struct node));
+    third=(struct node*)malloc(sizeof(struct node));
+    
+    first->data=1;
+    first->next=second;
+    
+    second->data=2;
+    second->next=third;
+    
+    third->data=3;
+    third->next=NULL;//data allocation
+
+    travel(first);
+  
+    //function call by keeping ptr at first 
+    first=delete(first,1);
+    printf("After deletion ");
+    //first bz head is imp,function 
+   travel(first);
+   
+    return 0;
+}
