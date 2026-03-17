88c1a6b19f0f9ac2bf4f54a6c4ae17be510bb096
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-03-17 14:26:04 +0530

    Update Check_if_Array_is_Sorted.c

--- Arrays/Check_if_Array_is_Sorted.c
+++ Arrays/Check_if_Array_is_Sorted.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-int main()
+int main() 
 {
-    int a[5]={1,2,3,4,5};
+    int a[]={1,2,3,4,5};
+    int n=sizeof(a)/sizeof(a[0]);
     int sorted=1;
     int *p=&a[0];
-    int  n=sizeof(a)/sizeof(a[0]);
-    while(p<a+n-1)
+    for(int i=0;i<n-1;i++)
     {
-        if(*p > *(p+1))
+        if ((*p) > *(p+1))
         {
             sorted=0;
-            
         }
-      p++;
+        p++;
     }
     if(sorted)
-    printf("yes");
+    printf("Yes");
     else
     printf("No");
     return 0;
