00165910ff7890940281fec4ee22ab7d4a9c6795
Sanchi2610 <sanchigaikwad2610@gmail.com>
2026-04-23 19:24:35 +0530

    Update Merge_Two_Array_In_Order.c

--- Arrays/Merge_Two_Array_In_Order.c
+++ Arrays/Merge_Two_Array_In_Order.c
@@ -1,37 +1,34 @@
 #include <stdio.h>
 int main() 
 {
-   int a[3]={45,1,26};
-   int b[3]={4,23,5};
+   int a[]={1,4,2,10,11,78};
    int n1=sizeof(a)/sizeof(a[0]);
+   int b[]={6,8,7,9,12,99};
    int n2=sizeof(b)/sizeof(b[0]);
-  for(int i=0;i<n1;i++)
-  {
-      for(int j=0;j<n1-1;j++)
-      {
-          if(a[j+1]<a[j])
-          {
-              int temp=a[j];
-              a[j]=a[j+1];
-              a[j+1]=temp;
-          }
-      }
-  }
+   int c[n1+n2];
+   for(int i=0;i<n1;i++)
+   {
+       c[i]=a[i];
+   }
+   for(int i=0;i<n2;i++)
+   {
+       c[n1+i]=b[i];
+   }
    for(int i=0;i<n1+n2;i++)
    {
-       for(int j=0;j<n1+n2-1;j++)
+       for(int j=0;j<n1+n2;j++)
        {
-           if(b[j+1]<b[j])
+           if(c[j+1]>c[j])
            {
-               int temp1=b[j];
-               b[j]=b[j+1];
-               b[j+1]=temp1;
+               int temp=c[j];
+               c[j]=c[j+1];
+               c[j+1]=temp;
            }
        }
    }
-  for(int i=0;i<n1+n2;i++)
-  {
-      printf(" %d",b[i]);
-  }
-   return 0;
+   for(int i=0;i<n1+n2;i++)
+   {
+       printf(" %d",c[i]);
+   }
+    return 0;
 }
